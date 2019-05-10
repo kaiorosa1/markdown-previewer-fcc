2e449228436f6804731b7ab1ebd11aa61273c9d8
kaiorosa1 <kaiosrosa@gmail.com>
2019-05-10 17:35:43 -0300

    add basic components created

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Basic Components Created
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -5,7 +6,38 @@ class App extends React.Component {
   render() {
     return (
       React.createElement("div", null,
-      React.createElement("h1", null, "H1 from the App Component")));
+      React.createElement("h1", null, "H1 from the App Component"),
+      React.createElement(Editor, null),
+      React.createElement(Previewer, null)));
+
+
+
+  }}
+
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      React.createElement("div", null,
+      React.createElement("h2", null, "Editor Component"),
+      React.createElement("textarea", { id: "editor" })));
+
+
+  }}
+
+
+class Previewer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      React.createElement("div", null,
+      React.createElement("div", { id: "preview" },
+      React.createElement("h2", null, "Preview Component"))));
 
 
 
