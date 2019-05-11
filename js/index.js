40ba7add170226cf864b3398ad810d8ea4a12860
kaiorosa1 <kaiosrosa@gmail.com>
2019-05-11 20:46:40 -0300

    add editor-previewer functionality

--- js/index.js
+++ js/index.js
@@ -2,13 +2,26 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      editorText: '' };
+
   }
+
+  changeText(newText) {
+    this.setState({
+      editorText: newText });
+
+  }
+
   render() {
+
     return (
       React.createElement("div", null,
       React.createElement("h1", null, "H1 from the App Component"),
-      React.createElement(Editor, null),
-      React.createElement(Previewer, null)));
+      React.createElement("div", { id: "main" },
+      React.createElement(Editor, { changeText: this.changeText.bind(this) }),
+      React.createElement(Previewer, { previewText: this.state.editorText }))));
+
 
 
 
@@ -19,11 +32,14 @@ class Editor extends React.Component {
   constructor(props) {
     super(props);
   }
+  sendText(e) {
+    this.props.changeText(e.target.value);
+  }
   render() {
     return (
-      React.createElement("div", null,
+      React.createElement("div", { id: "editor-box" },
       React.createElement("h2", null, "Editor Component"),
-      React.createElement("textarea", { id: "editor" })));
+      React.createElement("textarea", { onChange: this.sendText.bind(this), id: "editor" })));
 
 
   }}
@@ -35,9 +51,10 @@ class Previewer extends React.Component {
   }
   render() {
     return (
-      React.createElement("div", null,
+      React.createElement("div", { id: "preview-box" },
+      React.createElement("h2", null, "Preview Component"),
       React.createElement("div", { id: "preview" },
-      React.createElement("h2", null, "Preview Component"))));
+      React.createElement("h3", null, this.props.previewText))));
 
 
 
